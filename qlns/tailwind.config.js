fc7b4a35069fb19518fd2b109f69070476432481
Sĩ Ben <dangtiensi.vn@gmail.com>
2021-09-16 22:50:58 +0700

    siben

--- qlns/tailwind.config.js
+++ qlns/tailwind.config.js
@@ -1,21 +1,52 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-    mode: 'jit',
-    purge: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-    ],
-
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
-            },
-        },
+  purge: [
+    // prettier-ignore
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      red: colors.red,
+      orange: colors.orange,
+      yellow: colors.yellow,
+      green: colors.green,
+      gray: colors.blueGray,
+      indigo: {
+        100: '#e6e8ff',
+        300: '#b2b7ff',
+        400: '#7886d7',
+        500: '#6574cd',
+        600: '#5661b3',
+        800: '#2f365f',
+        900: '#191e38',
+      },
     },
-
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
-};
+    extend: {
+      borderColor: theme => ({
+        DEFAULT: theme('colors.gray.200', 'currentColor'),
+      }),
+      fontFamily: {
+        sans: ['Cerebri Sans', ...defaultTheme.fontFamily.sans],
+      },
+      boxShadow: theme => ({
+        outline: '0 0 0 2px ' + theme('colors.indigo.500'),
+      }),
+      fill: theme => theme('colors'),
+    },
+  },
+  variants: {
+    extend: {
+      fill: ['focus', 'group-hover'],
+    },
+  },
+  plugins: [],
+}
