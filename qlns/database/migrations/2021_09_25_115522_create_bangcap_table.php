38501b28a42a56fa56d3d51aa2950840b15e455f
Sĩ Ben <dangtiensi.vn@gmail.com>
2021-09-25 19:52:28 +0700

    commit

---
+++ qlns/database/migrations/2021_09_25_115522_create_bangcap_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBangcapTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bangcap', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('tenbc', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bangcap');
+    }
+}
