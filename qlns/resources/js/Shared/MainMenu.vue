b109f0411cec1af1bc80bd8d72abcd3dbf4417a1
Sĩ Ben <dangtiensi.vn@gmail.com>
2021-10-16 19:20:05 +0700

    commit

--- qlns/resources/js/Shared/MainMenu.vue
+++ qlns/resources/js/Shared/MainMenu.vue
@@ -61,6 +61,12 @@
       </inertia-link>
     </div>
     <div v-if="$page.props.auth.user.role == 2" class="mb-4">
+      <inertia-link class="flex items-center group py-3" :href="route('nhanvien')">
+        <icon name="users" class="w-4 h-4 mr-2" :class="isUrl('nhanvien') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'" />
+        <div :class="isUrl('nhanvien') ? 'text-white' : 'text-indigo-300 group-hover:text-white'">Nhân Viên</div>
+      </inertia-link>
+    </div>
+    <div v-if="$page.props.auth.user.role == 2" class="mb-4">
       <inertia-link class="flex items-center group py-3" :href="route('users')">
         <icon name="users" class="w-4 h-4 mr-2" :class="isUrl('users') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'" />
         <div :class="isUrl('users') ? 'text-white' : 'text-indigo-300 group-hover:text-white'">Người Dùng</div>
