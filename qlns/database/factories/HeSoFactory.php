9746438ed90c2ec2ae2d07dc0a93b5c0db0070e6
Sĩ Ben <dangtiensi.vn@gmail.com>
2021-10-25 21:30:06 +0700

    commit

--- qlns/database/factories/HeSoFactory.php
+++ qlns/database/factories/HeSoFactory.php
@@ -2,7 +2,7 @@
 
 namespace Database\Factories;
 
-use App\Models\Model;
+use App\Models\HeSo;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class HeSoFactory extends Factory
@@ -12,7 +12,7 @@ class HeSoFactory extends Factory
      *
      * @var string
      */
-    protected $model = Model::class;
+    protected $model = HeSo::class;
 
     /**
      * Define the model's default state.
