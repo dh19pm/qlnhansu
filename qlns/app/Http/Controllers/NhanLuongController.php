8187882073f4d49acb5d6448406f5f722250464f
Sĩ Ben <dangtiensi.vn@gmail.com>
2021-11-01 20:09:55 +0700

    commit

--- qlns/app/Http/Controllers/NhanLuongController.php
+++ qlns/app/Http/Controllers/NhanLuongController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\NhanLuong;
+use App\Models\NhanVien;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Request;
@@ -31,19 +32,52 @@ class NhanLuongController extends Controller
         ]);
     }
 
-    public function create()
+    public function create(NhanVien $nhanvien)
     {
-        return Inertia::render('NhanLuong/Create');
+        return Inertia::render('NhanLuong/Create', [
+            'nhanvien' => [
+                'id' => $nhanvien->id,
+                'hovaten' => $nhanvien->hovaten
+            ]
+        ]);
     }
 
-    public function store()
+    public function store(NhanVien $nhanvien)
     {
         Request::validate([
-            'tencm' => ['required', 'max:100', Rule::unique('nhanluong', 'tencm')]
+            'heso' => ['required', 'between:0,100.00'],
+            'phucap' => ['required', 'between:0,100.00'],
+            'khautru' => ['required', 'between:0,100.00'],
+            'luongcb' => ['required', 'integer'],
+            'mucluong' => ['required', 'integer'],
+            'ngaycongchuan' => ['required', 'integer'],
+            'ngaycong' => ['required', 'integer'],
+            'nghihl' => ['required', 'integer'],
+            'nghikhl' => ['required', 'integer'],
+            'thuong' => ['required', 'integer'],
+            'phat' => ['required', 'integer'],
+            'tamung' => ['required', 'integer'],
+            'thuclinh' => ['required', 'integer'],
+            'ngaynhan' => ['required', 'date'],
         ]);
 
         (new NhanLuong())->create([
-            'tencm' => Request::get('tencm')
+            'nhanvien_id' => $nhanvien->id,
+            'heso' => Request::get('heso'),
+            'phucap' => Request::get('phucap'),
+            'khautru' => Request::get('khautru'),
+            'luongcb' => Request::get('luongcb'),
+            'mucluong' => Request::get('mucluong'),
+            'ngaycongchuan' => Request::get('ngaycongchuan'),
+            'ngaycong' => Request::get('ngaycong'),
+            'nghihl' => Request::get('nghihl'),
+            'nghikhl' => Request::get('nghikhl'),
+            'thuong' => Request::get('thuong'),
+            'phat' => Request::get('phat'),
+            'tamung' => Request::get('tamung'),
+            'thuclinh' => Request::get('thuclinh'),
+            'thang' => date('m', strtotime(Request::get('ngaynhan'))),
+            'nam' => date('Y', strtotime(Request::get('ngaynhan'))),
         ]);
 
         return Redirect::route('nhanluong')->with('success', 'Đã tạo thành công.');
@@ -54,7 +88,10 @@ class NhanLuongController extends Controller
         return Inertia::render('NhanLuong/Edit', [
             'nhanluong' => [
                 'id' => $nhanluong->id,
-                'tencm' => $nhanluong->tencm,
+                'hovaten' => $nhanluong->nhanvien->hovaten,
+                'lydo' => $nhanluong->lydo,
+                'sotien' => $nhanluong->sotien,
+                'ngayung' => $nhanluong->nam . '-' . str_pad($nhanluong->thang, 2, '0', STR_PAD_LEFT),
                 'deleted_at' => $nhanluong->deleted_at,
             ],
         ]);
---
+++ qlns/resources/js/Pages/NhanLuong/Create.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <h1 class="mb-8 font-bold text-3xl">
+      <inertia-link class="text-indigo-400 hover:text-indigo-600" :href="route('nhanvien')">Nhân Viên</inertia-link>
+      <span class="text-indigo-400 font-medium">/</span>
+      <inertia-link class="text-indigo-400 hover:text-indigo-600" :href="route('nhanvien.edit', nhanvien.id)">{{ nhanvien.hovaten }}</inertia-link>
+      <span class="text-indigo-400 font-medium">/</span> Nhận Lương
+    </h1>
+    <div class="bg-white rounded-md shadow overflow-hidden">
+      <form @submit.prevent="store">
+        <div class="p-8 -mr-6 -mb-8 flex flex-wrap">
+          <text-input v-model="form.heso" :error="form.errors.heso" class="pr-6 pb-8 w-full lg:w-1/2" label="Hệ số lương" />
+          <text-input v-model="form.phucap" :error="form.errors.phucap" class="pr-6 pb-8 w-full lg:w-1/2" label="Phụ cấp" />
+          <text-input v-model="form.khautru" :error="form.errors.khautru" class="pr-6 pb-8 w-full lg:w-1/2" label="Khẩu trừ" />
+          <text-input v-model="form.luongcb" :error="form.errors.luongcb" class="pr-6 pb-8 w-full lg:w-1/2" type="number" label="Lương cơ bản" />
+          <text-input v-model="form.mucluong" :error="form.errors.mucluong" class="pr-6 pb-8 w-full lg:w-1/2" type="number" label="Mức lương" />
+          <text-input v-model="form.ngaycongchuan" :error="form.errors.ngaycongchuan" class="pr-6 pb-8 w-full lg:w-1/2" type="number" label="Ngày công chuẩn" />
+          <text-input v-model="form.ngaycong" :error="form.errors.ngaycong" class="pr-6 pb-8 w-full lg:w-1/2" type="number" label="Ngày công" />
+          <text-input v-model="form.nghihl" :error="form.errors.nghihl" class="pr-6 pb-8 w-full lg:w-1/2" type="number" label="Ngày nghỉ hưởng lương" />
+          <text-input v-model="form.nghikhl" :error="form.errors.nghikhl" class="pr-6 pb-8 w-full lg:w-1/2" type="number" label="Ngày nghỉ không hưởng lương" />
+          <text-input v-model="form.thuong" :error="form.errors.thuong" class="pr-6 pb-8 w-full lg:w-1/2" type="number" label="Tiền thưởng" />
+          <text-input v-model="form.phat" :error="form.errors.phat" class="pr-6 pb-8 w-full lg:w-1/2" type="number" label="Tiền phạt" />
+          <text-input v-model="form.tamung" :error="form.errors.tamung" class="pr-6 pb-8 w-full lg:w-1/2" type="number" label="Tạm ứng" />
+          <text-input v-model="form.thuclinh" :error="form.errors.thuclinh" class="pr-6 pb-8 w-full lg:w-1/2" type="number" label="Thực lĩnh" />
+          <text-input v-model="form.ngaynhan" :error="form.errors.ngaynhan" class="pr-6 pb-8 w-full lg:w-1/2" type="month" label="Tháng nhận" />
+        </div>
+        <div class="px-8 py-4 bg-gray-50 border-t border-gray-100 flex justify-end items-center">
+          <loading-button :loading="form.processing" class="btn-indigo" type="submit">Tạo Mới</loading-button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import Layout from '@/Shared/Layout'
+import TextInput from '@/Shared/TextInput'
+import SelectInput from '@/Shared/SelectInput'
+import LoadingButton from '@/Shared/LoadingButton'
+export default {
+  metaInfo: { title: 'Nhân Viên Nhận Lương' },
+  components: {
+    LoadingButton,
+    SelectInput,
+    TextInput,
+  },
+  layout: Layout,
+  props: {
+    nhanvien: Object
+  },
+  remember: 'form',
+  data() {
+    return {
+      form: this.$inertia.form({
+        heso: null,
+        phucap: null,
+        khautru: null,
+        luongcb: null,
+        mucluong: null,
+        ngaycongchuan: null,
+        ngaycong: null,
+        nghihl: null,
+        nghikhl: null,
+        thuong: null,
+        phat: null,
+        tamung: null,
+        thuclinh: null,
+        ngaynhan: null,
+      }),
+    }
+  },
+  methods: {
+    store() {
+        this.form.post(this.route('nhanluong.store', this.nhanvien.id))
+    },
+  },
+}
+</script>
