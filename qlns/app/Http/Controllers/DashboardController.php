92064f543728f6b6e272e23134afb588892d0c1b
Sĩ Ben <dangtiensi.vn@gmail.com>
2021-11-16 09:47:51 +0700

    commit

--- qlns/app/Http/Controllers/DashboardController.php
+++ qlns/app/Http/Controllers/DashboardController.php
@@ -2,15 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\NghiViec;
 use Inertia\Inertia;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        dd((new NghiViec())->checkNgayNghi(63, '2021-11-09'));
-        return '';
         return Inertia::render('Dashboard/Index');
     }
 }
